9a0f143939cc7481a79e77ed4db066951b6a101c
Sandeep Sharma <sandeep.sharma1@blackrock.com>
2019-03-18 16:50:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,4 +19,14 @@ function nowServing (katzDeliLine){
   
 }
 
+function currentLine (katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty."
+  }
+  var myString = `The line is currently: 1. ${katzDeliLine[0]}`
 
+  for(let i = 1; i<katzDeliLine.length;i++){
+  myString += `, ${i+1}. ${katzDeliLine[i]}`
+}
+return myString
+}
