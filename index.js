0b8d3cfe33426e3b18c449eb9775c98017a1db37
Sandeep Sharma <sandeep.sharma1@blackrock.com>
2019-03-18 16:42:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function takeANumber (katzDeliLine, name) {
 function nowServing (katzDeliLine){
   var name ;
   
-  if (katzDeliLine === 0){
+  if (katzDeliLine.length === 0){
     return "There is nobody waiting to be served!"
   }
 }
